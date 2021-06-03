5ce45fa8d51ea9dc2fa6021a0b06d8bdc4818fb3
Wenlin Huang <jackhwl@hotmail.com>
2021-06-03 08:35:29 -0400

    add curry function

---
+++ packages/babel-parser/test/curry-function.js
@@ -0,0 +1,13 @@
+// filename: packages/babel-parser/test/curry-function.js
+
+import { parse } from '../lib';
+
+function getParser(code) {
+  return () => parse(code, { sourceType: 'module' });
+}
+
+describe('curry function syntax', function() {
+  it('should parse', function() {
+    expect(getParser(`function @@ foo() {}`)()).toMatchSnapshot();
+  });
+});
---
+++ wenlin.md
@@ -0,0 +1,2 @@
+make bootstrap
+make build
